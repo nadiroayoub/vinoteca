64b5ba1c1214f763b2f1bb4da04ef3208054313e
Ayoub Nadir <nadiroayoub@gmail.com>
2022-02-27 22:49:48 +0100

    vinoteca ampliada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,61 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VinosComponent } from './ventas/pages/vinos/vinos.component';
+import { InicioComponent } from './ventas/pages/inicio/inicio.component';
+import { VinoItemComponent } from './ventas/pages/vino-item/vino-item.component';
+import { ErrorPageComponent } from './ventas/pages/error-page/error-page.component';
+import { ContactoComponent } from './ventas/pages/contacto/contacto.component';
+import { CrearVinoComponent } from './ventas/pages/crear-vino/crear-vino.component';
+import { LoginComponent } from './ventas/pages/login/login.component';
+import { RegisterComponent } from './ventas/pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'inicio',
+    component: InicioComponent,
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'vinos',
+    component: VinosComponent,
+  },
+  {
+    path: 'crearvino',
+    component: CrearVinoComponent,
+  },
+  {
+    path: 'vinos/:id',
+    component: VinoItemComponent,
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent,
+  },
+  {
+    path: '404',
+    component: ErrorPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Modulo personalizado
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Tercera parte
+import { AgmCoreModule } from '@agm/core';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PrimeNgModule,
+    SharedModule,
+    VentasModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBEVLuALTNr5HLHfj6OgQ53ah-HvTS0lG8',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
